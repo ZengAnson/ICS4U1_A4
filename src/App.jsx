9e5847d4bb2ed50d733ed151e556b1f95b6b3bc5
anson [home] <anson.zeng@student.tdsb.on.ca>
2024-11-17 16:55:01 -0500

    heron formula completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
-
 import './App.css'
-import Heron from './components/Heron.jsx'
-import Amb from './components/Amb.jsx'
-import Newton from './components/Newton.jsx'
-import Poly from './components/Poly.jsx'
+import Heron from './components/HeronForm.jsx'
+// import Amb from './components/Amb.jsx'
+// import Newton from './components/Newton.jsx'
+// import Poly from './components/Polynomial.jsx'
 
 
 function App() {
   return (
     <div>
       <Heron />
-      <Amb />
+      {/* <Amb />
       <Newton />
-      <Poly />
+      <Poly /> */}
     </div>
   )
 }
